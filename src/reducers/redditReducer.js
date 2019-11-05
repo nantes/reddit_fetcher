486c132a1f6e5ed03d4382bbe37614fe75747c67
Ivan Cetta <icetta@gmail.com>
2019-11-05 11:21:52 -0300

    fix refresh bug when not new

--- src/reducers/redditReducer.js
+++ src/reducers/redditReducer.js
@@ -16,20 +16,21 @@ export default function fetchRedditReducer(state = initialState, action) {
     case types.FETCH_REDDIT:
       return {...state, isLoading: true};
     case types.FETCH_REDDIT_SUCCESS:
+      //const sortedReddit = selectorSortReddit(action.reddit, state.sortValue);
       return {
         ...state,
         reddit: [...action.reddit],
-        sortedReddit: [...action.reddit],
+        sortedReddit: [...selectorSortReddit(action.reddit, state.sortValue)],
         isLoading: false,
       };
     case types.FETCH_REDDIT_ERROR:
       return {...state, error: action.error, isLoading: false};
     case types.SORT_REDDIT:
-      const sortedReddit = selectorSortReddit(state.reddit, action.sortValue);
+      //const sortedReddit = selectorSortReddit(state.reddit, action.sortValue);
       return {
         ...state,
         sortValue: action.sortValue,
-        sortedReddit: [...sortedReddit],
+        sortedReddit: [...selectorSortReddit(state.reddit, action.sortValue)],
       };
     case types.OPEN_ITEM:
       return {
