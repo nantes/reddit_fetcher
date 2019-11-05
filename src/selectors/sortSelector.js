bc8d4e8ffa093cebc4e1ba368c33fabf9cb22dce
Ivan Cetta <icetta@gmail.com>
2019-11-05 11:14:42 -0300

    implement sort based in controversial/hot/new/top

--- src/selectors/sortSelector.js
+++ src/selectors/sortSelector.js
@@ -1,14 +1,18 @@
-export default (reddit, value) => {
-  //TODO sort based in value or use new/hot/controversial/top jsons
-  switch (value) {
-    case 'new':
-      break;
-    case 'hot':
-      break;
-    case 'controversial':
-      break;
-    case 'top':
-      break;
-  }
+export default (reddit, sortValue) => {
+  reddit.sort((post1, post2) => {
+    switch (sortValue) {
+      case 'new':
+        return post1.data.created_utc < post2.data.created_utc ? -1 : 1;
+      case 'top':
+        return post1.data.score > post2.data.score ? -1 : 1;
+      case 'controversial':
+        return post1.data.score < post2.data.score &&
+          post1.data.num_comments > post2.data.num_comments
+          ? -1
+          : 1;
+      case 'hot':
+        return post1.data.num_comments > post2.data.num_comments ? -1 : 1;
+    }
+  });
   return reddit;
 };
