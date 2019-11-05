3558c527d73c9d1d64bd87951e4634d14ad59603
Ivan Cetta <icetta@gmail.com>
2019-11-05 13:19:03 -0300

    fix top and hot topics

--- src/selectors/sortSelector.js
+++ src/selectors/sortSelector.js
@@ -4,14 +4,14 @@ export default (reddit, sortValue) => {
       case 'new':
         return post1.data.created_utc < post2.data.created_utc;
       case 'top':
-        return post1.data.score > post2.data.score;
+        return post1.data.score < post2.data.score;
       case 'controversial':
         return (
           post1.data.score < post2.data.score &&
           post1.data.num_comments > post2.data.num_comments
         );
       case 'hot':
-        return post1.data.num_comments > post2.data.num_comments;
+        return post1.data.num_comments < post2.data.num_comments;
     }
   });
   return reddit;
